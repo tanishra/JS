ff3971ba91d25fed0deabc003a3b5c7773e7461a
Tanish Rajput <tanishrajput9@gmail.com>
2024-09-22 13:20:37 -0700

    added scopes.js file

---
+++ 03_basics/scopes.js
@@ -0,0 +1,16 @@
+// let a = 10;
+// const b = 20;
+// var c = 30;
+
+let a = 400;
+
+if(true){
+    let a = 10;
+    const b = 20;
+    var c = 30;
+    console.log("Inner a : ",a);
+}
+
+console.log(a);
+// console.log(b);
+console.log(c); // This will print value outside the block
