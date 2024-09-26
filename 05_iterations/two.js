3635c213403b1b59dd2975359c41254fb2ad42e0
Tanish Rajput <tanishrajput9@gmail.com>
2024-09-25 20:35:33 -0700

    added two.js file

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// while loop
+
+let i = 0;
+while(i <= 10){
+    console.log(`Value of i is ${i}`);
+    i = i + 2;    
+}
+
+const myArray = ["Ironman", "Hulk" , "Captain America", "Thor"];
+
+let j = 0;
+while(j < myArray.length){
+    console.log(myArray[j]);
+    j++;
+}
+
+
+// do-while loop
+
+let index = 0;
+do{
+    console.log(index);
+    index++;
+}while(index <= 10);
