0ef5afc7e66ab773a20415ae4e5041b7ab70fa40
Tanish Rajput <tanishrajput9@gmail.com>
2024-09-28 14:01:54 -0700

    added script.js file

---
+++ 07_projects/1-Color-Changer/script.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach((button) =>{
+    // console.log(button);
+    button.addEventListener('click',(e) => {
+        // console.log(e);
+        // console.log(e.target); // from where event initiated / started
+
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+        }else if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id;
+        }else if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }else if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
